cae0b517c0a05937fe5e840dd063d5406d326ea4
SaiGouthamVaddi <saigoutham.vgn@gmail.com>
2019-07-16 11:44:00 +0000

    Shopping App is created

---
+++ shoppingapp/src/main/java/com/visa/training/servlet/cart.java
@@ -0,0 +1,59 @@
+package com.visa.training.servlet;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.util.Arrays;
+import java.util.List;
+
+import javax.servlet.RequestDispatcher;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+
+
+@WebServlet("/cart")
+public class cart extends HttpServlet {
+	
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+			PrintWriter out = response.getWriter();
+			HttpSession session = request.getSession(true);
+			if(request.getParameterValues("bikebox")!=null)
+			{
+				String[] bikes = request.getParameterValues("bikebox") ;
+				session.setAttribute("bikes", Arrays.asList(bikes));
+				RequestDispatcher rd = request.getRequestDispatcher("books.html");
+				rd.forward(request, response);
+			} else if(request.getParameterValues("bookbox")!=null){
+				String[] books = request.getParameterValues("bookbox") ;
+				session.setAttribute("books", Arrays.asList(books));
+				RequestDispatcher rd = request.getRequestDispatcher("cars.html");
+				rd.forward(request, response);
+			} else if(request.getParameterValues("carbox")!=null){
+				String[] carss = request.getParameterValues("carbox") ;
+				session.setAttribute("cars", Arrays.asList(carss));
+				
+				List<String> books = (List<String>) session.getAttribute("books");
+				List<String> bikes = (List<String>) session.getAttribute("bikes");
+				List<String> cars = (List<String>) session.getAttribute("cars");
+				
+				out.println(books);
+				out.println(cars);
+				out.println(bikes);
+				
+				session.invalidate();
+			} 
+			
+			
+			
+	}
+
+	
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		doGet(request, response);
+	}
+
+}
